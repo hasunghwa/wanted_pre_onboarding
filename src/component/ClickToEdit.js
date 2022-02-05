7194bac89aab22456b9178192c1950e8946cc2e3
DESKTOP-ICDGUQJ\하성화 <tjdghk3920@naver.com>
2022-02-05 13:19:03 +0900

    ClickToEdit upload

---
+++ src/component/ClickToEdit.js
@@ -0,0 +1,39 @@
+import { useEffect, useRef, useState } from "react";
+import styled from "styled-components";
+
+const Label = styled.label`
+`;
+
+const Input = styled.input`
+  margin: 10px 10px;
+  width: 150px;
+  height: 30px;
+  text-align: center;
+  border: 1px solid rgba(0, 0, 0, 0.2);
+`;
+
+function ClickToEdit() {
+  const [name, setName] = useState("김코딩");
+  const [age, setAge] = useState(20);
+  const nameRef = useRef();
+  const ageRef = useRef();
+  useEffect(() => {
+    if(nameRef.current.value !== name)
+      nameRef.current.value = name;
+    if(ageRef.current.value !== age)
+      ageRef.current.value = age;
+  },[name, age]);
+
+  return (
+    <>
+      <Label>이름</Label>
+      <Input ref={nameRef} onBlur={(e) => setName(e.target.value)}/>
+      <br />
+      <Label>나이</Label>
+      <Input ref={ageRef} onBlur={(e) => setAge(e.target.value)}/>
+      <p>이름 {name} 나이 {age}</p>
+    </>
+  )
+}
+
+export default ClickToEdit;
